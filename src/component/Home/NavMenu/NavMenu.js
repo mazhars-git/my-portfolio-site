b79cfa962fb4e2c36d13c0715c8fef79296459e0
mazhar <mazharulislam611@gmail.com>
2023-12-12 07:32:45 +0600

    update

--- src/component/Home/NavMenu/NavMenu.js
+++ src/component/Home/NavMenu/NavMenu.js
@@ -6,7 +6,7 @@ const NavMenu = () => {
         <div>
            <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
                 <div class="container-fluid main-menu">
-                    <a class="navbar-brand" href="#"><span className="logo">MAZHAR</span></a>
+                    <a class="navbar-brand" href="#"><span className="logo">MAZ<span class="text-info">HAR</span></span></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
