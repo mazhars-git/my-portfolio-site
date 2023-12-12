c5b6c115759fa4dee8d3c0ee0fe9d241b597585f
mazhar <mazharulislam611@gmail.com>
2023-12-12 07:39:42 +0600

    class to className

--- src/component/Home/NavMenu/NavMenu.js
+++ src/component/Home/NavMenu/NavMenu.js
@@ -4,28 +4,28 @@ import './NavMenu.css';
 const NavMenu = () => {
     return (
         <div>
-           <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
-                <div class="container-fluid main-menu">
-                    <a class="navbar-brand" href="#"><span className="logo">MAZ<span class="text-info">HAR</span></span></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+           <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+                <div className="container-fluid main-menu">
+                    <a className="navbar-brand" href="#"><span className="logo">MAZ<span className="text-dark">HAR</span></span></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item mr-3">
-                            <a class="nav-link active" href="#home">Home</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item mr-3">
+                            <a className="nav-link active" href="#home">Home</a>
                         </li>
-                        <li class="nav-item mr-3">
-                            <a class="nav-link" href="#about">About</a>
+                        <li className="nav-item mr-3">
+                            <a className="nav-link" href="#about">About</a>
                         </li>
-                        <li class="nav-item mr-3">
-                            <a class="nav-link" href="#project">Portfolio</a>
+                        <li className="nav-item mr-3">
+                            <a className="nav-link" href="#project">Portfolio</a>
                         </li>
-                        <li class="nav-item mr-3">
-                            <a class="nav-link" href="#services">Services</a>
+                        <li className="nav-item mr-3">
+                            <a className="nav-link" href="#services">Services</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#contact">Contact</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#contact">Contact</a>
                         </li>
                     </ul>
                     
