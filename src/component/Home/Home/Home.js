ddece10d8800de901610a58091d998a1eb557167
mazhars-git <mazharulislam611@gmail.com>
2025-05-03 01:06:07 +0600

    update

--- src/component/Home/Home/Home.js
+++ src/component/Home/Home/Home.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import AboutMe from '../AboutMe/AboutMe';
-import Contact from '../Contact/Contact';
-import Footer from '../Footer/Footer';
-import MySkill from '../MySkill/MySkill';
-import Projects from '../Projects/Projects';
-import Services from '../Services/Services';
-import HeaderContent from '../HeaderContent/HeaderContent';
-import NavMenu from '../NavMenu/NavMenu';
+import React from "react";
+import AboutMe from "../AboutMe/AboutMe";
+import Contact from "../Contact/Contact";
+import Footer from "../Footer/Footer";
+import MySkill from "../MySkill/MySkill";
+import Projects from "../Projects/Projects";
+import Services from "../Services/Services";
+import HeaderContent from "../HeaderContent/HeaderContent";
+import NavMenu from "../NavMenu/NavMenu";
+import MySkills from "../MySkill/MySkills";
 
 const Home = () => {
-    return (
-        <section id="home" className="home-section bg-color">
-            <NavMenu />
-            <HeaderContent />
-            <AboutMe></AboutMe>
-            <MySkill></MySkill>
-            <Services></Services>
-            <Projects></Projects>
-            <Contact></Contact>
-            <Footer></Footer>
-        </section>
-    );
+  return (
+    <section id="home" className="home-section bg-color">
+      <NavMenu />
+      <HeaderContent />
+      <AboutMe></AboutMe>
+      <MySkill></MySkill>
+      <MySkills />
+      <Services></Services>
+      <Projects></Projects>
+      <Contact></Contact>
+      <Footer></Footer>
+    </section>
+  );
 };
 
 export default Home;
--- src/component/Home/MySkill/MySkills.js
+++ src/component/Home/MySkill/MySkills.js
@@ -1,11 +1,133 @@
-import React from 'react';
+import React from "react";
 
 const MySkills = () => {
-    return (
-        <div>
-            <h1>My Skills</h1>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-offset-3 col-md-6">
+          <h3 class="progress-title">HTML5</h3>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style={{ width: "25%" }}
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              25%
+            </div>
+          </div>
         </div>
-    );
+
+        <div className="col-md-offset-3 col-md-6">
+          <h3 class="progress-title">HTML5</h3>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style={{ width: "25%" }}
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              25%
+            </div>
+          </div>
+        </div>
+        <div className="col-md-offset-3 col-md-6">
+          <h3 class="progress-title">HTML5</h3>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style={{ width: "25%" }}
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              25%
+            </div>
+          </div>
+        </div>
+        <div className="col-md-offset-3 col-md-6">
+          <h3 class="progress-title">HTML5</h3>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style={{ width: "25%" }}
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              25%
+            </div>
+          </div>
+        </div>
+        <div className="col-md-offset-3 col-md-6">
+          <h3 class="progress-title">HTML5</h3>
+          <div class="progress orange">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style={{ width: "25%" }}
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              25%
+            </div>
+          </div>
+        </div>
+        <div className="col-md-offset-3 col-md-6">
+          <h3 class="progress-title">HTML5</h3>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style={{ width: "25%" }}
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              25%
+            </div>
+          </div>
+        </div>
+        <div className="col-md-offset-3 col-md-6">
+          <h3 class="progress-title">HTML5</h3>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style={{ width: "25%" }}
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              25%
+            </div>
+          </div>
+        </div>
+        <div className="col-md-offset-3 col-md-6">
+          <h3 class="progress-title">HTML5</h3>
+          <div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style={{ width: "25%" }}
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              25%
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MySkills;
